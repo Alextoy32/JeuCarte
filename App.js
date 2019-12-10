4c9441f5cd3f5f6526dc44a47f015986f27b73f3
Alexis Boutan <alexis.boutan@hotmail.fr>
2019-12-10 14:42:35 +0100

    wait what

--- App.js
+++ App.js
@@ -7,7 +7,9 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import Search from './Components/Search'
+import DiceApp from './Components/DiceApp'
 
 import firebase from '@react-native-firebase/app';
 
@@ -18,10 +20,7 @@ import firebase from '@react-native-firebase/app';
 //    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-});
+
 
 const firebaseCredentials = Platform.select({
   ios: 'https://invertase.link/firebase-ios',
@@ -31,14 +30,13 @@ const firebaseCredentials = Platform.select({
 type Props = {};
 
 export default class App extends Component<Props> {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <DiceApp/>
         {!firebase.apps.length && (
-          <Text style={styles.instructions}>
+          <Text>
             {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
           </Text>
         )}
@@ -50,18 +48,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
